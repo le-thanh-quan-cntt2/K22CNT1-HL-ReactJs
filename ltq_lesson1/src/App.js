52f2df2607d3c568674fff15560f0a9fe934768e
le-thanh-quan-cntt2 <quanlekrn01@gmail.com>
2025-09-03 13:51:52 +0700

    Add ltq_lesson1 as normal folder

---
+++ ltq_lesson1/src/App.js
@@ -0,0 +1,19 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="Container">
+      <h1>Thông tin sinh viên</h1>
+      <hr/>
+      <div className='alert alert-danger'>
+        <p>Mã sinh viên: 2210900057</p>
+        <p>Họ và tên: Lê Thanh Quan </p>
+        <p>Email: quanlekrn01@gmai.com</p>
+        <p>Phone: 0357695529</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
